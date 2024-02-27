e9cc0c7e91e26a33ecec6495fdf0a9df55bd9255
Nabin yadav <nabiny501@gmail.com>
2024-02-27 14:00:46 +0545

    edited

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const port = 4000;
 app.get('/', (req, res) => {
     res.send("hello world")
 })
-app.get('/nabin',(req, res)=>{
+app.get('/nabin', (req, res) => {
     res.send("navigated to /nabin function ")
 })
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.send("login page")
 })
 
 app.listen(process.env.PORT, () => {
     console.log(`listening to port ${process.env.PORT}`);
 })
+
+module.exports = app;
